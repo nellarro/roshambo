4a9e666475b52f30c715acfe1f7cae93b667ab63
Jenell Pizarro <nellarro@gmail.com>
2016-07-29 17:11:13 -0400

    Why did my "Let's Play" button disappear?! Console code works in console but don't know how to make the js, html, css talk to each other. None of the buttons are working.

--- main.js
+++ main.js
@@ -1,36 +1,52 @@
-let userChoice = //add event listener for clicking the picture
-let compChoice = Math.random()
+const possibleThrows = ['Rock', 'Paper', 'Scissors']
+ let userChoice = ''
+ let computerChoice = ''
 
-if (compChoice <= 0.33)
-  window.log('Computer chose: rock') // How do I keep track of this?
- else if (0.66 > compChoice >= 0.34) {
-  window.log('Computer chose: paper')// see comment above
-} else {
-  window.log('Computer chose: scissors')// are these printed out in the console?
-}// or do I print them in a seperate table somewhere on the window? window.log?
+const init = function() {
+    document.querySelector('.output').textContent = 'Let\'s Play!'
+    let rock = document.querySelector('.rock')
+    let paper = document.querySelector('.paper')
+    let scissors = document.querySelector('.scissors')
 
-const compare = function (choice1, choice2) {
-  if (choice1 === choice2) {
-    return 'The result is a tie!'// do not log an engagement
-  } if (choice1  === 'scissors') {
-    if (choice2 === 'paper') {
-      return 'scissors wins'// log an engagement --> check to see if bout/match
-    } else {
-      return 'paper wins'// log an engagement --> checkt to see if bout/match
-    }
-  } else if (choice1 === 'paper') {
-    if (choice2 === 'rock') {
-      return 'paper wins'// log an engagement --> check for match/bout
-    }
-  } else if (choice1 === 'rock') {
-    if (choice2 === 'scissors') {
-      return 'rock wins'// log an engagement --> check for match/bout
-    }
+Rock.addEventListener('click', function() {
+    userChoice = 'Rock'
+    begin(userChoice)
   }
-}
-console.log(compare(userChoice, compChoice))
 
-  document.querySelector('.output').textContent = 'Let\'s Play!'
-
+/paper.addEventListener('click', function() {
+     userChoice = 'Paper'
+     begin(userChoice)
+  }
+/scissors.addEventListener('click', function() {
+     userChoice = 'Scissors'
+     begin(userChoice)
+  }
 
 document.addEventListener('DOMContentLoaded', init)
+
+let compare = function(choice1, choice2){
+  if(choice1 === choice2){
+    return 'TIE!'
+  }if (choice1 === 'scissors') {
+    if (choice2 === 'paper') {
+      return 'scissors wins' // log this to the scoreboard
+    }else {
+      return 'paper wins' // log this to the scoreboard
+    }else if (choice1 === 'paper'){
+      if (choice2 === 'rock') {
+        return 'paper wins' // log this to the scoreboard
+      }else {
+        return 'rock wins' // log this to the scoreboard
+      }else if (choice1 === 'rock') {
+        if (choice2 === 'scissors'){
+          return 'rock wins' // log this to the scoreboard
+        }else {
+          return 'scissors wins'// log this to the scoreboard
+        }
+      }
+    }
+compare(userChoice, computerChoice)
+
+
+
+//document.addEventListener('DOMContentLoaded', init)
