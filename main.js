38810ae5a47079b28f8e4653781dd03f7d57ce7d
Jenell Pizarro <nellarro@gmail.com>
2016-07-30 15:39:22 -0400

    compChoice logging rock at random! yay

--- main.js
+++ main.js
@@ -8,23 +8,55 @@ const initialize = () => {
 document.addEventListener('DOMContentLoaded', initialize)
 
 let rock = document.querySelector('.input button.rock')
-console.log(rock)
 
 let scissors = document.querySelector('.input button.scissors')
-console.log(scissors)
 
 let paper = document.querySelector('.input button.paper')
-console.log(paper)
 
-let test = () => {
-  alert('Working')
+let startGame = () => {
+  computerChoice = Math.random()
+  if (computerChoice < 0.33) {
+    computerChoice = rock
+    console.log('rock')
 }
-rock.addEventListener('click', test)
-scissors.addEventListener('click', test)
-paper.addEventListener('click', test)
+}
+
+rock.addEventListener('click', startGame)
+// scissors.addEventListener('click', test2)
+// paper.addEventListener('click', test3)
+
 
 // document.addEventListener('click', startGame)
 //
 // let startGame = () => {
 //   document.querySelector('.input').button = userChoice
 // }
+
+// <input>
+// <button class="es">Click 1 for Spanish</button>
+// <button class="en">Click 2 for English</button>
+
+// <script>
+//   let language = ''
+//   const greet = function(){
+//     let heading = document.querySelector('h1')
+//     let nameInput = document.querySelector('input')
+//     let word = ''
+//     if(language === 'English'){
+//       word = 'Hello, '
+//     } else {
+//       word = 'Hola, '
+//     }
+//     heading.innerText = word + nameInput.value
+//   }
+//   let englishButton = document.querySelector('.en')
+//   englishButton.addEventListener('click', function(){
+//     language = 'English'
+//     greet()
+//   })
+//   let spanishButton = document.querySelector('.es')
+//   spanishButton.addEventListener('click', function(){
+//     language = 'Spanish'
+//     greet()
+//   })
+// </script>
